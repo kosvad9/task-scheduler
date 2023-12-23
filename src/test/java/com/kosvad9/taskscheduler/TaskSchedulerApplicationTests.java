829080ed79ff1bafe5f8f2fd0fb7f9db3f784032
Vadim <kosvad9@gmail.com>
2023-12-23 16:41:37 +0300

    Initial commit

---
+++ src/test/java/com/kosvad9/taskscheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosvad9.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
